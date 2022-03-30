b2d6c6b542deb101be92c8ce8e71aa69eeff82fd
Antonio Filho <amhfilho@gmail.com>
2022-03-30 15:40:48 +0100

    Estrutura inicial do projeto

---
+++ desafio1/src/main/java/com/amhfilho/sevendaysofcode/App.java
@@ -0,0 +1,11 @@
+package com.amhfilho.sevendaysofcode;
+
+/**
+ * Desafio 1 - 7DaysOfCode
+ * Aplicação de linha de comando (CLI) para consulta dos top 250 filmes do IMDB
+ */
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
